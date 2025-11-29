ff033f9af30c5c69989a3fab3e77a317563c1d6c
Md Husain Thekiya <hussainthekiya@gmail.com>
2025-11-29 16:41:32 +0530

    [RELEASE] - app v.1.0.0 | added backend url

--- WaqtApp/src/constants/api.ts
+++ WaqtApp/src/constants/api.ts
@@ -1,4 +1,1 @@
-import { Platform } from 'react-native';
-
-export const API_BASE_URL =
-  Platform.OS === 'android' ? 'http://192.168.0.104:4003/api' : 'http://192.168.0.104:4003/api';
+export const API_BASE_URL = 'https://waqt-backend.vercel.app/api';
